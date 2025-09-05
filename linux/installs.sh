47a7d704e203fb4611723912c4e7a3de2fdd6a00
Buster Collings <busterc@gmail.com>
2025-09-05 17:19:09 -0500

    Update profile script header and fix NVM repository URL in installs script

--- linux/installs.sh
+++ linux/installs.sh
@@ -14,7 +14,7 @@ sudo apt-get install -y nginx
 
 # NVM
 export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
   cd "$NVM_DIR"
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
